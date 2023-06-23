24d71e40a7555a5b2cc01b1f0f0df551adf9c0da
ffnascimento <feliperfnascimento@gmail.com>
2023-06-23 16:07:17 -0300

    third

--- src/tut/src/Tutorial.cpp
+++ src/tut/src/Tutorial.cpp
@@ -3,6 +3,8 @@
 #include "ord/OpenRoad.hh"
 #include "utl/Logger.h"
 #include "grt/GlobalRouter.h"
+#include "stt/SteinerTreeBuilder.h"
+
 
 #include <iostream>
 
@@ -22,20 +24,79 @@ Tutorial::printHello()
 
   odb::dbBlock *block = db_->getChip()->getBlock(); //pega o bloco
   auto cellNumber = db_->getChip()->getBlock()->getInsts().size();
-  int blockDelta = 0;
+  //int blockDelta = 0;
 
-
-  std::cout<<"CellNumber: "<< cellNumber << std::endl;
+  std::cout<<"No. of cells in block: "<< cellNumber << std::endl;
   //int delta[cellNumber];
 
-  std::map <std::string, int> mapCellDeltas;
+
+  std::map <std::string, int> mapNetDeltas; //mapa de nets e deltas
+  
+  std::map <std::string, std::pair<int,int>> netWlLookup; //mapa de nets e hpwl,stwl
+  //netWlLookup[net->getName()] = std::make_pair(hpwl,stwl);
+  //netWlLookup[net->getName()].first; //retorna hpwl
+  //netWlLookup[net->getName()].second; //retorna stwl
+
+  /* sugerido na reuniao:
+  std::vector<std::pair<int,std::string>> aa; 
+  
+  aa.push_back(std::make_pair(10,"oioioi"));
+  aa.push_back(std::make_pair(5,"aiaia"));
+
+  
+
+  std::sort(aa.begin(),aa.end());
+  for(auto data: aa){
+    auto inteiro = data.first;
+    auto text = data.second;
+  }*/
+  std::vector<std::pair<int,std::string>> delta;
+
+  for (auto net: block->getNets()){ //desenvolver a partir daqui
+
+      //std::cout<< "cells map size: " << netWlLookup.size() << std::endl; //
+      //std::cout<< "delta map size: " << mapNetDeltas.size() << std::endl;
+
+      auto netName = net->getName(); //pega o nome desta celula
+      std::cout<<"      NetName: "<< netName << std::endl;
+
+      int hpwl, stwl;
+
+      netWlLookup[netName] = std::make_pair(hpwl,stwl);
+    
+      hpwl = calc_HPWL(net);
+      netWlLookup[netName].first = hpwl; //insere hpwl no pair
+
+      stwl = grt_->computeNetWirelength(net); //transformei esse metodo pra public - WL da net
+      std::cout<<"               WL: "<< stwl << std::endl; //calculo do wirelength da net
+      netWlLookup[netName].second = stwl; //insere stwl no pair
+
+      //logger_->report("HPWL: "+std::to_string(hpwl));  
+
+      int netDelta = stwl - hpwl; //calculo do delta da net/pino
+      std::cout<<"        Net Delta: "<< netDelta << std::endl;
+      mapNetDeltas[netName] = netDelta;
 
 
-  std::map <std::string, std::pair<int,int>> netWlLookup;
-  //netWlLookup[net->getName()] = std::make_pair(stwl,hpwl);
-  //netWlLookup[net->getName()].first; //retorna stwl
-  //netWlLookup[net->getName()].second; //retorna hpwl
+      delta.push_back(std::make_pair(netDelta,netName));
 
+      std::cout<< "            cell map size: " << netWlLookup.size() << std::endl;
+      std::cout<< "           delta map size: " << mapNetDeltas.size() << std::endl;
+
+  }
+
+  std::sort(delta.begin(),delta.end());
+
+  for(auto data: delta){
+    auto inteiro = data.first;
+    auto text = data.second;
+    std::cout<<" Delta: "<< inteiro <<" Net name: " << text <<std::endl;
+  }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#if(0)
   for(auto cell: block->getInsts()) //pega a cell
   {
     auto cellName = cell->getName(); //pega o nome deste pino
@@ -53,73 +114,86 @@ Tutorial::printHello()
       if(pinName != "VDD" && pinName != "VSS") //se o pino nao eh VDD ou VSS
       {
         if(net) //se existe uma net ligada no pino
-        {
-          auto netName = net->getName(); //pega o nome desta celula
-          std::cout<<"      NetName: "<< netName << std::endl;
-
-        
-          // calculo do hpwl (substituir por uso de metodo depois)
-          //logger_->report("Net: "+net->getName());
-          #if(0)
-          int xll = std::numeric_limits<int>::max();
-          int yll = std::numeric_limits<int>::max();
-          int xur = std::numeric_limits<int>::min();
-          int yur = std::numeric_limits<int>::min();
-          for(auto iterm : net->getITerms())
           {
-            int x=0, y=0;
-            iterm->getAvgXY(&x, &y);
-            
-            xur = std::max(xur, x);
-            yur = std::max(yur, y);
-            xll = std::min(xll, x);
-            yll = std::min(yll, y);
-          }
-          int width = std::abs(xur-xll);
-          int height = std::abs(yur-yll);
-          
-          int hpwl = width + height; //calculo do hpwl da net/pino
-          std::cout<<"             HPWL: "<< hpwl << std::endl;
-          #endif
-          
-          int hpwl = calc_HPWL(net);
+            if( !(netWlLookup.count(net->getName()) )) //se a net ainda nao foi contabilizada e inserida no mapa
+            { 
+            //std::cout<< "cells map size: " << netWlLookup.size() << std::endl; //
+            //std::cout<< "delta map size: " << mapNetDeltas.size() << std::endl;
 
-          int Wirelength = grt_->computeNetWirelength(net); //transformei esse metodo pra public - WL da net
-          std::cout<<"               WL: "<< Wirelength << std::endl; //calculo do wirelength da net
-          //logger_->report("HPWL: "+std::to_string(hpwl));  
+            auto netName = net->getName(); //pega o nome desta celula
+            std::cout<<"      NetName: "<< netName << std::endl;
 
-          int netDelta = Wirelength - hpwl; //calculo do delta da net/pino
-          std::cout<<"        Net Delta: "<< netDelta << std::endl;
-          cellDelta += netDelta;
+            int hpwl, stwl;
+
+            netWlLookup[netName] = std::make_pair(hpwl,stwl);
+          
+            hpwl = calc_HPWL(net);
+            netWlLookup[netName].first = hpwl; //insere hpwl no pair
+
+            stwl = grt_->computeNetWirelength(net); //transformei esse metodo pra public - WL da net
+            std::cout<<"               WL: "<< stwl << std::endl; //calculo do wirelength da net
+            netWlLookup[netName].second = stwl; //insere stwl no pair
+
+            //logger_->report("HPWL: "+std::to_string(hpwl));  
+
+            int netDelta = stwl - hpwl; //calculo do delta da net/pino
+            std::cout<<"        Net Delta: "<< netDelta << std::endl;
+            mapNetDeltas[netName] = netDelta;
+
+            cellDelta += netDelta;
+
+            std::cout<< "            cell map size: " << netWlLookup.size() << std::endl;
+            std::cout<< "           delta map size: " << mapNetDeltas.size() << std::endl;
+
+          } //if not mapped
+
+          else std::cout<<"      Net "<< net->getName()<< " already mapped"<<std::endl; // net ja mapeada
 
         } //if net
 
-        else std::cout<<"      No Net connected"<<std::endl; //caso pino sem net
+        else std::cout<<"      No Net connected"<<std::endl; // pino sem net
       } //if VDD
 
-      //else std::cout<<"      VDD/VSS"<<std::endl; //caso VDD/VSS
+      //else std::cout<<"      VDD/VSS"<<std::endl; // VDD/VSS
 
     } //for pin
   blockDelta += cellDelta;
-  //mapCellDeltas[cellDelta] = cell;
+  //mapNetDeltas[cellDelta] = cell;
 
   } //for cell
+  
   std::cout<<"Total Block Delta: "<< blockDelta << std::endl;
 
-  {
+  std::cout<< "cells map size: " << netWlLookup.size() << std::endl;
+  std::cout<< "delta map size: " << mapNetDeltas.size() << std::endl;
 
+
+  for(auto pair:netWlLookup){
+    auto net = pair.first;
+    auto data = pair.second;//pair
+
+    auto hpwl = data.first;
+    auto stwl = data.second;
 
   }
+#endif
+  //std::cout<<mapNetDeltas[0] << std::endl;
 
-
+  //for (auto i = mapNetDeltas.begin(); i != mapNetDeltas.end(); i++){
+  // std::cout<< "delta map pos "<< i <<": " << mapNetDeltas[i] << std::endl;
+  //}
   
+
+  //for(int i; i > mapNetDeltas.end() ; i++){
+  //  std::cout << mapNetDeltas[i] <<" , "<< ;
+
 } //metodo
   //grt_->reportNetWireLength(net,true,false,false,""); metodo void, retorna no logger
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int
-calc_HPWL(odb::dbNet* net)
+Tutorial::calc_HPWL(odb::dbNet* net)
 {
   int xll = std::numeric_limits<int>::max();
   int yll = std::numeric_limits<int>::max();
@@ -238,6 +312,34 @@ Tutorial::~Tutorial()
 
 
 }
+
+
+          #if(0)
+          // calculo do hpwl (substituir por uso de metodo depois)
+          //logger_->report("Net: "+net->getName());
+          //logger_->report("HPWL: "+std::to_string(hpwl));  
+
+          
+          int xll = std::numeric_limits<int>::max();
+          int yll = std::numeric_limits<int>::max();
+          int xur = std::numeric_limits<int>::min();
+          int yur = std::numeric_limits<int>::min();
+          for(auto iterm : net->getITerms())
+          {
+            int x=0, y=0;
+            iterm->getAvgXY(&x, &y);
+            
+            xur = std::max(xur, x);
+            yur = std::max(yur, y);
+            xll = std::min(xll, x);
+            yll = std::min(yll, y);
+          }
+          int width = std::abs(xur-xll);
+          int height = std::abs(yur-yll);
+          
+          int hpwl = width + height; //calculo do hpwl da net/pino
+          std::cout<<"             HPWL: "<< hpwl << std::endl;
+          #endif
 
 /*
 void
